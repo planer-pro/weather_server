99f1900913c8ed48c20a5226b40d8f4249726648
planer-pro <planer.reg@gmail.com>
2017-07-04 20:05:46 +0300

    add adress in description

--- serv.js
+++ serv.js
@@ -3,12 +3,12 @@ var express = require('express');
 var app = express();
 // Parse 
 var mqtt_url = url.parse('mqtt://m21.cloudmqtt.com:12307');
-var auth = ['ilgudqkb','OI7RNW8-uu2U'];
+var auth = ['ilgudqkb', 'OI7RNW8-uu2U'];
 var url = "mqtt://" + mqtt_url.host;
-var datatemp=[];
-var datahum=[];
-var datapress=[];
-var dataalt=[];
+var datatemp = [];
+var datahum = [];
+var datapress = [];
+var dataalt = [];
 
 var options = {
   port: mqtt_url.port,
@@ -21,8 +21,8 @@ var options = {
 var client = mqtt.connect(url, options);
 
 client.on('connect', () => {
-	client.subscribe('#')
-	client.publish('outdoor/sensors/bme280_getState', '1')
+  client.subscribe('#')
+  client.publish('outdoor/sensors/bme280_getState', '1')
   /*client.subscribe('outdoor/sensors/bme280_temp')
   client.subscribe('outdoor/sensors/bme280_hum')
   client.subscribe('outdoor/sensors/bme280_press')
@@ -32,56 +32,53 @@ client.on('connect', () => {
 client.on('message', (topic, message) => {
   switch (topic) {
     case 'outdoor/sensors/bme280_temp':
-    datatemp.push({
-    	message: message, 
-    	time: new Date()
-    })
-    if(datatemp.length>43200)
-    {
-    	datatemp.shift()
-    }
+      datatemp.push({
+        message: message,
+        time: new Date()
+      })
+      if (datatemp.length > 43200) {
+        datatemp.shift()
+      }
       break;
 
     case 'outdoor/sensors/bme280_hum':
-    datahum.push({
-    	message: message, 
-    	time: new Date()
-    })
-    if(datahum.length>43200)
-    datahum.shift()
+      datahum.push({
+        message: message,
+        time: new Date()
+      })
+      if (datahum.length > 43200)
+        datahum.shift()
       break;
 
     case 'outdoor/sensors/bme280_press':
-    datapress.push({
-    	message: message, 
-    	time: new Date()
-    })
-    if(datapress.length>43200)
-    datapress.shift()
+      datapress.push({
+        message: message,
+        time: new Date()
+      })
+      if (datapress.length > 43200)
+        datapress.shift()
       break;
 
     case 'outdoor/sensors/bme280_alt':
-    dataalt.push({
-    	message: message, 
-    	time: new Date()
-    })
-    if(dataalt.length>43200)
-    dataalt.shift()
+      dataalt.push({
+        message: message,
+        time: new Date()
+      })
+      if (dataalt.length > 43200)
+        dataalt.shift()
       break;
   }
 })
 
-app.get('/', function (req, res) 
-{
-	//if (!message) return;
-	if (datatemp.length==0 || datahum.length==0 || datapress.length==0
-		|| dataalt.length==0)
-	{
+app.get('/', function (req, res) {
+  //if (!message) return;
+  if (datatemp.length == 0 || datahum.length == 0 || datapress.length == 0
+    || dataalt.length == 0) {
     res.send('Oops, something does wrong)');
     return;
-}
+  }
 
-    var mess = `
+  var mess = `
         <!DOCTYPE html>
         <html>
          <head>
@@ -91,25 +88,25 @@ app.get('/', function (req, res)
          <body>
          `;
 
-        mess += datatemp[datatemp.length - 1].time + '<br><br>';
-        mess +='Temperature: ' + datatemp[datatemp.length - 1].message + ' degrees <br>';
-		mess += 'Humidity: ' + datahum[datahum.length - 1].message + ' % <br>';
-		mess +='Pressure: ' + datapress[datapress.length - 1].message + ' hPa <br>';
-		mess += 'Altitude: ' + dataalt[dataalt.length - 1].message + ' m <br>';
+  mess += datatemp[datatemp.length - 1].time + ' Kolasa 8 Str. Minsk, Belarus' + '<br><br>';
+  mess += 'Temperature: ' + datatemp[datatemp.length - 1].message + ' degrees <br>';
+  mess += 'Humidity: ' + datahum[datahum.length - 1].message + ' % <br>';
+  mess += 'Pressure: ' + datapress[datapress.length - 1].message + ' hPa <br>';
+  mess += 'Altitude: ' + dataalt[dataalt.length - 1].message + ' m <br>';
 
-   /* mess += `
-          <form>
-           <p>Введите температуру 0-100 градусов:</p>
-           <p><input type='number' size='3' name='num' min='0' max='100' value='0'></p>
-           <p>0 - выключить всё</p>
-           <input type='checkbox' name='termopot' value='1'>Включить поддержание нагрева<Br>
-           <p><input type='submit' value='Выполнить'></p>
-          </form>
-         </body>
-        </html>
-        `;*/
+  /* mess += `
+         <form>
+          <p>Введите температуру 0-100 градусов:</p>
+          <p><input type='number' size='3' name='num' min='0' max='100' value='0'></p>
+          <p>0 - выключить всё</p>
+          <input type='checkbox' name='termopot' value='1'>Включить поддержание нагрева<Br>
+          <p><input type='submit' value='Выполнить'></p>
+         </form>
+        </body>
+       </html>
+       `;*/
 
-		res.send(mess);
+  res.send(mess);
 });
 
 app.listen(process.env.PORT || 3000, function () {
