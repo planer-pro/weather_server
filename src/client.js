69107dcd315ef2adf8caa360256647038ea41d7b
derwish-pro <derwish.pro@gmail.com>
2017-11-23 23:27:08 +0300

    add chart

--- src/client.js
+++ src/client.js
@@ -1,9 +1,11 @@
 import HeadInfo from "./head-info"
+import Chart from "./chart"
 
 var app = new Vue({
     el: '#app',
     data: {},
     components: {
-        "head-info": HeadInfo
+        "head-info": HeadInfo,
+        "chart": Chart
     }
 })
